c54273a7ea64defc8492b174cc0ecd5799a22a9d
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2021-03-20 14:12:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## zusb
+A port of the library [rusb](https://github.com/a1ien/rusb) to Zig.
+
+### Usage
+* Add `libusb-1.0/libusb.h` to the include path in `build.zig`.
+* Link libusb 1.0 in `build.zig`.
+
+### TODO
+* Add rusb doc comments.
---
+++ zusb.zig
@@ -0,0 +1,12 @@
+pub usingnamespace @import("src/config_descriptor.zig");
+pub usingnamespace @import("src/constants.zig");
+pub usingnamespace @import("src/context.zig");
+pub usingnamespace @import("src/device_descriptor.zig");
+pub usingnamespace @import("src/device_handle.zig");
+pub usingnamespace @import("src/device_list.zig");
+pub usingnamespace @import("src/device.zig");
+pub usingnamespace @import("src/endpoint_descriptor.zig");
+pub usingnamespace @import("src/error.zig");
+pub usingnamespace @import("src/fields.zig");
+pub usingnamespace @import("src/interface_descriptor.zig");
+pub usingnamespace @import("src/transfer.zig");
